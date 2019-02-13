3790951557223b5fc3e9c959baf22c2e07aab7d8
spythesky <kris@spythesky.com>
2019-02-13 15:45:06 +0100

    commit

---
+++ DataFactory/datafactory.py
@@ -0,0 +1,8 @@
+from azure.common.credentials import ServicePrincipalCredentials
+from azure.mgmt.resource import ResourceManagementClient
+from azure.mgmt.datafactory import DataFactoryManagementClient
+from azure.mgmt.datafactory.models import *
+from datetime import datetime, timedelta
+import time
+
+
